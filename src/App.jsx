5fb8cf70c7772445db3b13124b88ae609c422790
Ayush Tamang <tamangayush052@gmail.com>
2024-08-14 20:09:23 +0545

    project setupt

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+function App() {
+  return (
+    <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      PopularProducts
+    </section>
+    <section className="padding">
+      SuperQuantity 
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+    Special offer
+    </section>
+    <section className="padding">
+    CustomerReviews
+    </section>
+    <section className="padding">
+    Special offer
+    </section>
+    <section className="padding">
+    Special offer
+    </section>
+  </main>
+  )
+}
+
+export default App
